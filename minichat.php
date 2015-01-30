f326e843e18ac7d19e534752c3a48eb5924134c0
koalacoding <koalacoding@net-c.fr>
2015-01-30 02:46:52 +0100

    minichat.php now shows the last 15 messages posted.

--- minichat.php
+++ minichat.php
@@ -19,5 +19,29 @@
 			</form>
 		</center>
 
+		<?php
+			// We try to connect to the SQL database.
+			try
+			{
+				$bdd = new PDO('mysql:host=localhost;dbname=test2;charset=utf8', 'root', '');
+			}
+			// In case of error.
+			catch(Exception $e)
+			{
+			        die('Error : '.$e->getMessage());
+			}
+
+			// We get all the entries from the minichat table.
+			$request = $bdd->query('SELECT * FROM minichat ORDER BY id DESC LIMIT 0, 15');
+
+			/* We use a for loop to show every message one by one.
+			htmlspecialchars is used to prevent the execution of HTML code
+			if a nickname or a message contains HTML tags. */
+			while ($messages = $request->fetch()) {
+				echo '<p><strong>' . htmlspecialchars($messages['nickname'])
+				 . '</strong> : ' . htmlspecialchars($messages['message']) . '<p>';
+			}				
+		?>
+
 	</body>
 </html>
